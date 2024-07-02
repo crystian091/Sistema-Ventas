fed03cb5f551c15436b0d1ad816c2728a8c6b298
crystian091 <165923272+crystian091@users.noreply.github.com>
2024-07-02 11:05:09 -0500

    Add files via upload

---
+++ SistemaVentas/SistemaVentas/Form4.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace SistemaVentas
+{
+    partial class Form4
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGenerar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNrodeBoleta = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtFecha = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtCodCliente = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtCodVendedor = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtAnulado = new System.Windows.Forms.TextBox();
+            this.dgvBoleta = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvBoleta)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnGenerar
+            // 
+            this.btnGenerar.BackColor = System.Drawing.Color.Transparent;
+            this.btnGenerar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGenerar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGenerar.ForeColor = System.Drawing.Color.Khaki;
+            this.btnGenerar.Location = new System.Drawing.Point(64, 263);
+            this.btnGenerar.Name = "btnGenerar";
+            this.btnGenerar.Size = new System.Drawing.Size(109, 50);
+            this.btnGenerar.TabIndex = 0;
+            this.btnGenerar.Text = "Generar";
+            this.btnGenerar.UseVisualStyleBackColor = false;
+            this.btnGenerar.Click += new System.EventHandler(this.btnGenerar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.SeaShell;
+            this.label1.Location = new System.Drawing.Point(27, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 22);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "N° boleta:";
+            // 
+            // txtNrodeBoleta
+            // 
+            this.txtNrodeBoleta.Location = new System.Drawing.Point(132, 35);
+            this.txtNrodeBoleta.Name = "txtNrodeBoleta";
+            this.txtNrodeBoleta.Size = new System.Drawing.Size(100, 22);
+            this.txtNrodeBoleta.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.SeaShell;
+            this.label2.Location = new System.Drawing.Point(30, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 22);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Fecha:";
+            // 
+            // txtFecha
+            // 
+            this.txtFecha.Location = new System.Drawing.Point(132, 73);
+            this.txtFecha.Name = "txtFecha";
+            this.txtFecha.Size = new System.Drawing.Size(100, 22);
+            this.txtFecha.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.SeaShell;
+            this.label3.Location = new System.Drawing.Point(30, 112);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 22);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Cliente:";
+            // 
+            // txtCodCliente
+            // 
+            this.txtCodCliente.Location = new System.Drawing.Point(132, 112);
+            this.txtCodCliente.Name = "txtCodCliente";
+            this.txtCodCliente.Size = new System.Drawing.Size(100, 22);
+            this.txtCodCliente.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.SeaShell;
+            this.label4.Location = new System.Drawing.Point(29, 150);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(102, 22);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Vendedor:";
+            // 
+            // txtCodVendedor
+            // 
+            this.txtCodVendedor.Location = new System.Drawing.Point(132, 150);
+            this.txtCodVendedor.Name = "txtCodVendedor";
+            this.txtCodVendedor.Size = new System.Drawing.Size(100, 22);
+            this.txtCodVendedor.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.SeaShell;
+            this.label5.Location = new System.Drawing.Point(30, 187);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(89, 22);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Anulado:";
+            // 
+            // txtAnulado
+            // 
+            this.txtAnulado.Location = new System.Drawing.Point(132, 187);
+            this.txtAnulado.Name = "txtAnulado";
+            this.txtAnulado.Size = new System.Drawing.Size(100, 22);
+            this.txtAnulado.TabIndex = 10;
+            // 
+            // dgvBoleta
+            // 
+            this.dgvBoleta.BackgroundColor = System.Drawing.Color.LightSteelBlue;
+            this.dgvBoleta.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvBoleta.Location = new System.Drawing.Point(274, 44);
+            this.dgvBoleta.Name = "dgvBoleta";
+            this.dgvBoleta.RowHeadersWidth = 51;
+            this.dgvBoleta.RowTemplate.Height = 24;
+            this.dgvBoleta.Size = new System.Drawing.Size(657, 329);
+            this.dgvBoleta.TabIndex = 11;
+            // 
+            // Form4
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.ForestGreen;
+            this.ClientSize = new System.Drawing.Size(943, 450);
+            this.Controls.Add(this.dgvBoleta);
+            this.Controls.Add(this.txtAnulado);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtCodVendedor);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtCodCliente);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtFecha);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtNrodeBoleta);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnGenerar);
+            this.Name = "Form4";
+            this.Text = "Boleta";
+            this.Load += new System.EventHandler(this.Form4_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvBoleta)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGenerar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNrodeBoleta;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtFecha;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtCodCliente;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtCodVendedor;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtAnulado;
+        private System.Windows.Forms.DataGridView dgvBoleta;
+    }
+}
---
+++ SistemaVentas/SistemaVentas/Form4.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SistemaVentas
+{
+    public partial class Form4 : Form
+    {
+        public Form4()
+        {
+            InitializeComponent();
+        }
+
+        VentasDataContext ventas = new VentasDataContext();
+
+        private void Form4_Load(object sender, EventArgs e)
+        {
+            Listar();
+        }
+
+        private void Listar()
+        {
+            try
+            {
+                var consulta = from C in ventas.Boleta
+                               select C;
+
+                dgvBoleta.DataSource = consulta.ToList(); // Convierte la consulta a una lista y asigna al DataSource del DataGridView
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al cargar los datos: " + ex.Message);
+            };
+
+        }
+
+        private void btnGenerar_Click(object sender, EventArgs e)
+        {
+            Boleta boleta = new Boleta();
+            try
+            {
+                boleta.NroBoleta = int.Parse(txtNrodeBoleta.Text);
+                boleta.Fecha = DateTime.Parse(txtFecha.Text); // Asumiendo que Fecha es de tipo DateTime
+                boleta.CodCliente = txtCodCliente.Text;
+                boleta.CodVendedor = txtCodVendedor.Text;
+                boleta.Anulado = bool.Parse(txtAnulado.Text); // Conversión a bool (si Anulado es de tipo bool)
+
+                ventas.Boleta.InsertOnSubmit(boleta);
+                ventas.SubmitChanges(); // Confirmar la transacción
+
+                txtNrodeBoleta.Clear();
+                txtFecha.Clear();
+                txtCodVendedor.Clear();
+                txtCodCliente.Clear();
+                txtAnulado.Clear();
+                txtNrodeBoleta.Focus();
+                Listar();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+    }
+}
+
